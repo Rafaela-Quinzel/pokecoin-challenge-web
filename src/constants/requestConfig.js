819f12408d98cbda44ac9c31de1a000bd7661c67
Rafaela Quinzel <71155947+Rafaela-Quinzel@users.noreply.github.com>
2022-08-22 19:09:34 -0300

    Update requestConfig.js

--- src/constants/requestConfig.js
+++ src/constants/requestConfig.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:27017/";
+export const BASE_URL = process.env.REACT_APP_API_URL;
 
 const token = `Bearer ${localStorage.getItem('token')}`;
 export const axiosConfig = {
