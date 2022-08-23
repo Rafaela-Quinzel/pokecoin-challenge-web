3bbd6bfda99190ea9d6d5039ee4cf2bd297e93b7
Your Name <you@example.com>
2022-08-22 22:00:29 -0300

    necessary settings

--- src/constants/requestConfig.js
+++ src/constants/requestConfig.js
@@ -3,6 +3,7 @@ export const BASE_URL = process.env.REACT_APP_API_URL;
 const token = `Bearer ${localStorage.getItem('token')}`;
 export const axiosConfig = {
     headers: {
-        authorization: token
+        Authorization: token,
+        "Content-Type": "application/json"
     }
 }
